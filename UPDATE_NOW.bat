0d7aea91524158c69e46f9821d3be4baa3c29574
CaptainPOS <loveaser763@gmail.com>
2026-09-15 23:22:56 +0300

    Add Railway deployment config

--- UPDATE_NOW.bat
+++ UPDATE_NOW.bat
@@ -2,15 +2,15 @@
 title Captain POS Update
 color 0A
 echo ============================================
-echo   Captain POS - Update
-echo   D to G
+echo   Captain POS - Update + Deploy
+echo   D -^> G -^> GitHub -^> SnapDeploy
 echo ============================================
 echo.
-echo [1/3] Syncing code from D to G...
+echo [1/4] Syncing code from D to G...
 robocopy "D:\CaptainPOS" "G:\نسخه العميل" /E /XO /XD "D:\CaptainPOS\node_modules" "D:\CaptainPOS\release" "D:\CaptainPOS\dist" "D:\CaptainPOS\backups" "D:\CaptainPOS\uploads" "D:\CaptainPOS\.git" "D:\CaptainPOS\New folder" "D:\CaptainPOS\public\zesty-data" "D:\CaptainPOS\public\jameety-data" "D:\CaptainPOS\src\components\jameety\data" /XF database.sqlite database.sqlite-shm database.sqlite-wal *.log /NFL /NDL /NJH /NJS
 echo       - Done!
 echo.
-echo [2/3] Building installer...
+echo [2/4] Building installer...
 echo       - This will take 2-3 minutes...
 cd /d "D:\CaptainPOS"
 call "C:\Program Files\nodejs\npm.cmd" run ship:win
@@ -21,20 +21,27 @@ if %errorlevel% neq 0 (
 )
 echo       - Done!
 echo.
-echo [3/3] Copying installer to update folder...
+echo [3/4] Copying installer to update folder...
 if not exist "G:\تحديث_للعميل" mkdir "G:\تحديث_للعميل"
 xcopy /Y /D "D:\CaptainPOS\release\CaptainPOS-1.0.0-Portable.exe" "G:\تحديث_للعميل\"
 xcopy /Y /D "D:\CaptainPOS\release\CaptainPOS-1.0.0-Windows.exe" "G:\تحديث_للعميل\"
-if %errorlevel% neq 0 (
-    echo       - Copy error! Press any key to exit
-    pause
-    exit /b 1
-)
 echo       - Done!
+echo.
+echo [4/4] Pushing to GitHub (auto-deploy to SnapDeploy)...
+cd /d "D:\CaptainPOS"
+git add -A
+git commit -m "Update %date% %time%"
+git push origin main
+if %errorlevel% neq 0 (
+    echo       - Push error! Check your internet connection.
+) else (
+    echo       - Done! SnapDeploy will auto-deploy.
+)
 echo.
 echo ============================================
 echo   Update complete!
-echo   Installer is in: G:\update_for_client
+echo   - Installer: G:\update_for_client
+echo   - Demo: captain-pos-demo-6be6b.snapdeploy.app
 echo ============================================
 echo.
 echo Press any key to exit...
